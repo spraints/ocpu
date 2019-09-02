d512edf4dd426809f0ca8a259c0e348a83b3674e
Matt Burke <spraints@gmail.com>
2019-09-02 11:52:23 -0400

    just rewrite it

--- ocasm.c
+++ ocasm.c
@@ -2,141 +2,87 @@
 #include <string.h>
 #include <stdlib.h>
 
-int parse(const char *line, char *insts)
-{
-    return 0;
-#ifdef OCASM_ALL
-    char insts[4];
-    int i = 0;
-    int t = 0;
-    
-    int word;
-    
-    while (t <= 4) {
-        
-        word = "";
-        
-        printf("ps wd\n");
-        
-        while (1) {
-            if (i < strlen(line)) {
-                if (line[i] != " ") {
-                    printf("%s\n",line[i]);
-                    strcat(word,line[i]);
-                    ++i;
-                    printf("inc i\n");
-                }
-                else {
-                    printf("br\n");
-                    break;
-                }
-            }
-            else {
-                printf("br\n");
-                break;
-            }
+#define BOOL int
+
+BOOL ocblank(char c) {
+    return c == ' ' || c == '\r' || c == '\n' || c == '\0';
+}
+
+char *next_token(char *s) {
+    if (ocblank(*s)) {
+        return NULL;
+    }
+
+    while(!ocblank(*s)) {
+        s++;
+    }
+
+    while(ocblank(*s)) {
+        if (*s == '\0') {
+            break;
         }
-        ++i;
-        printf("inc i\n");
-        printf("add wd %s\n",word);
-        insts[t] = word;
-        ++t;
-        printf("inc t\n");
+        *s = '\0';
+        s++;
     }
-    printf("rtn\n");
-    return(insts, t);
-#endif
+
+    return s;
 }
 
-const char *parseasm(const char* line) {
-    printf("ps ln %s\n", line);
-    //char **parsed = parse(line);
-#ifdef OCASM_ALL
-    char ln[4];
-    
-    int C = code[0];
-    
-    printf("ps inst %s\n", C);
-    if (C == "ld") {
-        strcat(ln, "00");
-    }
-    
-    if (C == "ldm") {
-        strcat(ln, "01");
-    }
-    
-    if (C == "ldr") {
-        strcat(ln, "02");
-    }
-    
-    if (C == "st") {
-        strcat(ln, "10");
-    }
-    
-    if (C == "std") {
-        strcat(ln, "11");
-    }
-    
-    if (C == "noop") {
-        strcat(ln, "FE");
-    }
-    
-    if (C == "halt") {
-        strcat(ln, "FF");
-    }
-    
-    printf("asm ln\n");
-    strcat(ln, " "); strcat(ln,code[1]); strcat(ln," "); strcat(ln,code[2]);
-    strcat(ln," "); strcat(ln,code[3]);
-    
-    printf("got %s\n", ln);
-    
-    printf("rtn\n");
-    return(ln);
-#endif
-    return "";
-}
+void process(char *line, FILE *output) {
+    char *token = line;
+    char *rest = next_token(token);
+    if (rest == NULL)
+        return;
 
-#define LINE_LEN 16
+    if (strcmp(token, "ld") == 0) {
+        fputs("00", output);
+    }
+    
+    if (strcmp(token, "ldm") == 0) {
+        fputs("01", output);
+    }
+    
+    if (strcmp(token, "ldr") == 0) {
+        fputs("02", output);
+    }
+    
+    if (strcmp(token, "st") == 0) {
+        fputs("10", output);
+    }
+    
+    if (strcmp(token, "std") == 0) {
+        fputs("11", output);
+    }
+    
+    if (strcmp(token, "noop") == 0) {
+        fputs("FE", output);
+    }
+    
+    if (strcmp(token, "halt") == 0) {
+        fputs("FF", output);
+    }
+
+    for(rest = next_token(token = rest); rest != NULL; rest = next_token(token = rest)) {
+        fprintf(output, " %s", token);
+    }
+
+    fputs("\n", output);
+}
 
 int main(int argc, char **argv) {
-    printf("init\n");
-    
     if (argc != 2) { /* If there is no [FILE] argument */
         printf("Usage: ocasm [FILE]\n"); /* Print usage */
         exit(0); /* Exit */
     }
-    
-    printf("open files\n");
 
     FILE *infile = fopen(argv[1],"r"); /* Open the input file */
     FILE *outfile = fopen("out.oca","w"); /* Open the output file */
-    
-    char lines[65535][LINE_LEN]; /* File probably won't be this long, but
-        a single file should not be longer than 65535 lines. */
-    char line[LINE_LEN] = {0}; /* Lines should only be about 16 characters max */
-    
-    int i = 0;
-    
-    printf("read\n");
 
-    while (fgets(line, sizeof(line), infile)) { /* Magic */
-        strncpy(lines[i], line, LINE_LEN);
-        printf("get ln %d\n", i);
-        i++;
+    char line[1000];
+    while (fgets(line, sizeof(line), infile)) {
+        process(line, outfile);
     }
-    
-    printf("close\n");
 
     fclose(infile);
-    
-    printf("parse\n");
-
-    for (int ln=0; ln < i; ++ln) {
-        fprintf(outfile, "%s\n", parseasm(lines[ln]));
-    }
-    
     fclose(outfile);
-    
-    printf("done.\n");
 }
